9baafc8da3b7561abe8c82dc94abd26e06712a51
Cristiano Leal <cristianofran@hotmail.com>
2023-07-09 18:53:40 -0300

    Alteração para janela abrir maximizada.

--- meu-primeiro-script.py
+++ meu-primeiro-script.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
+driver.maximize_window()
 driver.get("https://google.com.br/")
 
 search_box = driver.find_element(by=By.NAME, value="q")
@@ -11,3 +12,4 @@ search_box.send_keys("iPhone 14 Pro Max 1TB")
 search_box.send_keys(Keys.ENTER)
 
 time.sleep(30)
+driver.quit()
